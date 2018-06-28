8909627896b261d12489fddc6d2c8094c26ef289
Ross Brown <ross@cremalab.com>
2018-06-28 16:49:45 -0500

    Update deps, remove set height on header image

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -1,26 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import Header from './Header'
-import Box from 'react-boxen'
-import styled from 'styled-components'
-import Footer from './Footer'
-import { blue } from '../config/colors'
-import Head from 'next/head'
-import Menu from './Menu'
-import Nav from './Nav'
-import {action as toggleMenu} from 'redux-burger-menu'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Header from "./Header";
+import Box from "react-boxen";
+import styled from "styled-components";
+import Footer from "./Footer";
+import { blue } from "../config/colors";
+import Head from "next/head";
+import Menu from "./Menu";
+import Nav from "./Nav";
+import { action as toggleMenu } from "redux-burger-menu";
 
 const HeaderContainer = styled.div`
   z-index: 10;
   position: fixed;
   width: 100%;
-`
+`;
 
 const StyledLayout = styled(Box)`
-  font-family: 'Palanquin', sans-serif;
+  font-family: "Palanquin", sans-serif;
 
-  h3, h2 {
+  h3,
+  h2 {
     margin-bottom: 0;
   }
 
@@ -29,9 +30,11 @@ const StyledLayout = styled(Box)`
     margin-top: 0;
   }
 
-  ul, ol, blockquote, a {
+  ul,
+  ol,
+  blockquote,
+  a {
     font-weight: 100;
-
   }
 
   p a {
@@ -41,24 +44,27 @@ const StyledLayout = styled(Box)`
     }
   }
 
-  h1, h2, h3 {
+  h1,
+  h2,
+  h3 {
     color: ${props => props.color || blue};
     font-weight: 900;
   }
-`
+`;
 
 const StyledWrapper = styled.div`
   padding-top: 3em;
-`
+`;
 
-const MainLayout = (props) => (
-  <StyledLayout color={props.config.main_color} id='outer-container'>
+const MainLayout = props => (
+  <StyledLayout color={props.config.main_color} id="outer-container">
     <Head>
-      <title>{ `${props.page.slug === 'home' ? '' : `${props.page.name} | `}Eureka Mastering` }</title>
+      <title>{`${
+        props.page.slug === "home" ? "" : `${props.page.name} | `
+      }Eureka Mastering`}</title>
       <meta name="description" content={props.config.meta_description} />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-
 
     <HeaderContainer>
       <Header
@@ -71,15 +77,25 @@ const MainLayout = (props) => (
     </HeaderContainer>
 
     <StyledWrapper>
-      <Menu config={props.config} right pageWrapId={ 'page-wrap' } outerContainerId={ 'outer-container' }>
-        <Nav onNavigate={() => props.toggleMenu(false)} layout='vertical' inPage={props.route.pathname === '/'} route={props.route}/>
+      <Menu
+        config={props.config}
+        right
+        pageWrapId={"page-wrap"}
+        outerContainerId={"outer-container"}
+      >
+        <Nav
+          onNavigate={() => props.toggleMenu(false)}
+          layout="vertical"
+          inPage={props.route.pathname === "/"}
+          route={props.route}
+        />
       </Menu>
       <div id="page-wrap">{props.children}</div>
     </StyledWrapper>
 
     <Footer color={props.config.footer_color} contact={props.contact} />
   </StyledLayout>
-)
+);
 
 MainLayout.propTypes = {
   page: PropTypes.object,
@@ -88,11 +104,11 @@ MainLayout.propTypes = {
   route: PropTypes.object,
   children: PropTypes.array,
   toggleMenu: PropTypes.func,
-  menu: PropTypes.object,
-}
+  menu: PropTypes.object
+};
 
-const mapStateToProps = (state) => ({
-  menu: state.burgerMenu,
-})
+const mapStateToProps = state => ({
+  menu: state.burgerMenu
+});
 
-export default connect(mapStateToProps, { toggleMenu })(MainLayout)
+export default connect(mapStateToProps, { toggleMenu })(MainLayout);
--- pages/index.js
+++ pages/index.js
@@ -1,95 +1,115 @@
-import Link from 'next/link'
-import styled from 'styled-components'
-import withRedux from 'next-redux-wrapper'
-import Box from 'react-boxen'
-import { initStore, startClock, addCount, serverRenderClock } from '../store'
-import fetch from 'isomorphic-unfetch'
-import ReactHowler from 'react-howler'
+import Link from "next/link";
+import styled from "styled-components";
+import withRedux from "next-redux-wrapper";
+import Box from "react-boxen";
+import { initStore, startClock, addCount, serverRenderClock } from "../store";
+import fetch from "isomorphic-unfetch";
+import ReactHowler from "react-howler";
 
-import MainLayout from '../components/MainLayout'
-import { apiUrl } from '../config/urls'
-import media from '../config/media'
-import { gray, black } from '../config/colors'
+import MainLayout from "../components/MainLayout";
+import { apiUrl } from "../config/urls";
+import media from "../config/media";
+import { gray, black } from "../config/colors";
 
-import Service from '../components/Service'
-import ImageCarousel from '../components/ImageCarousel'
-import Heading from '../components/Text/Heading'
-import ConstrainedContainer from '../components/ConstrainedContainer'
-import ProjectGrid from '../components/ProjectGrid'
-import SiteTitle from '../components/SiteTitle'
-import Social from '../components/Social'
+import Service from "../components/Service";
+import ImageCarousel from "../components/ImageCarousel";
+import Heading from "../components/Text/Heading";
+import ConstrainedContainer from "../components/ConstrainedContainer";
+import ProjectGrid from "../components/ProjectGrid";
+import SiteTitle from "../components/SiteTitle";
+import Social from "../components/Social";
 
-import { setAudioLoading, pauseAudio } from '../store'
+import { setAudioLoading, pauseAudio } from "../store";
 
 const Section = styled.div`
   margin-top: 2em;
   margin-bottom: 2em;
-`
+`;
 
-const Index = ({page, services, equipment, projects, contact, config, state, setAudioLoading, pauseAudio, url}) => (
+const Index = ({
+  page,
+  services,
+  equipment,
+  projects,
+  contact,
+  config,
+  state,
+  setAudioLoading,
+  pauseAudio,
+  url
+}) => (
   <MainLayout contact={contact} config={config} route={url} page={page}>
-    {state.audio_url &&
+    {state.audio_url && (
       <ReactHowler
-      src={state.audio_url}
-      playing={state.audio_playing}
-      onPlay={() => setAudioLoading(false)}
-      onLoad={() => setAudioLoading(false)}
-      xhrWithCredentials={true}
-      onEnd={() => pauseAudio()} />
-    }
-    <div id='about'>
-      <ImageCarousel images={page.images} opacity='0.6' height={'270px'} abstract={true}/>
-      <ConstrainedContainer padding={'1em'}>
+        src={state.audio_url}
+        playing={state.audio_playing}
+        onPlay={() => setAudioLoading(false)}
+        onLoad={() => setAudioLoading(false)}
+        xhrWithCredentials={true}
+        onEnd={() => pauseAudio()}
+      />
+    )}
+    <div id="about">
+      <ImageCarousel images={page.images} opacity="0.6" abstract={true} />
+      <ConstrainedContainer padding={"1em"}>
         <Social color={config.main_color} />
-        <div dangerouslySetInnerHTML={{__html: page.content}} />
+        <div dangerouslySetInnerHTML={{ __html: page.content }} />
       </ConstrainedContainer>
     </div>
-    <Section id='discography'>
-      <ConstrainedContainer padding={'1em'}>
+    <Section id="discography">
+      <ConstrainedContainer padding={"1em"}>
         <Heading>Discography</Heading>
       </ConstrainedContainer>
       <ProjectGrid items={projects} config={config} />
-      <ConstrainedContainer padding={'1em'}>
-        <Box childAlign='flex-end'>
-          <p>see <Link prefetch href='/discography'><a>my full discography &rarr;</a></Link></p>
+      <ConstrainedContainer padding={"1em"}>
+        <Box childAlign="flex-end">
+          <p>
+            see{" "}
+            <Link prefetch href="/discography">
+              <a>my full discography &rarr;</a>
+            </Link>
+          </p>
         </Box>
       </ConstrainedContainer>
     </Section>
-    <Section id='equipment'>
-      <ConstrainedContainer padding={'1em'}>
+    <Section id="equipment">
+      <ConstrainedContainer padding={"1em"}>
         <Heading>Equipment</Heading>
       </ConstrainedContainer>
-      { equipment.images.length > 0 &&
-        <div style={{margin: '2em 0'}} >
+      {equipment.images.length > 0 && (
+        <div style={{ margin: "2em 0" }}>
           <ImageCarousel
             images={equipment.images}
             fadeDuration={1.5}
             delay={4000}
-            />
+          />
         </div>
-      }
-      <ConstrainedContainer padding={'1em'}>
-        <div dangerouslySetInnerHTML={{__html: equipment.content}} />
+      )}
+      <ConstrainedContainer padding={"1em"}>
+        <div dangerouslySetInnerHTML={{ __html: equipment.content }} />
       </ConstrainedContainer>
     </Section>
-    <Section id='services'>
-      <ConstrainedContainer padding={'1em'}>
+    <Section id="services">
+      <ConstrainedContainer padding={"1em"}>
         <Heading>Services</Heading>
-        { services.map(c => <Service key={c._id} service={c} />)}
+        {services.map(c => <Service key={c._id} service={c} />)}
       </ConstrainedContainer>
     </Section>
   </MainLayout>
-)
+);
 
-Index.getInitialProps = async function (context) {
-  const { id } = context.query
-  const res = await fetch(`${apiUrl}/pages/home`)
-  const page = await res.json()
-  return page
-}
+Index.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`${apiUrl}/pages/home`);
+  const page = await res.json();
+  return page;
+};
 
-const mapStateToProps = (state) => ({
-  state: state.app,
-})
+const mapStateToProps = state => ({
+  state: state.app
+});
 
-export default withRedux(initStore, mapStateToProps, { setAudioLoading, pauseAudio })(Index)
+export default withRedux(initStore, mapStateToProps, {
+  setAudioLoading,
+  pauseAudio
+})(Index);
