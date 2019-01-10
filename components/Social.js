6e69c7e641f936004f4eb952b2ec828769e5c859
Ross Brown <ross@cremalab.com>
2019-01-10 10:37:11 -0600

    removing facebook

--- components/Social.js
+++ components/Social.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Box from 'react-boxen'
-import { SocialIcon } from 'react-social-icons'
-import { blue } from '../config/colors'
+import React from "react";
+import PropTypes from "prop-types";
+import Box from "react-boxen";
+import { SocialIcon } from "react-social-icons";
+import { blue } from "../config/colors";
 
-const iconSize = 30
+const iconSize = 30;
 
 const style = {
   height: `${iconSize}px`,
-  width: `${iconSize}px`,
-}
+  width: `${iconSize}px`
+};
 
-const Social = ({color}) => (
-  <Box childDirection='row' childJustify='flex-end' childSpacing={'10px'}>
-    <SocialIcon style={style} color={color || blue} url="https://www.facebook.com/mikenoltemastering" />
-    <SocialIcon style={style} color={color || blue} url="http://instagram.com/eurekamastering" />
+const Social = ({ color }) => (
+  <Box childDirection="row" childJustify="flex-end" childSpacing={"10px"}>
+    <SocialIcon
+      style={style}
+      color={color || blue}
+      url="http://instagram.com/eurekamastering"
+    />
   </Box>
-)
+);
 
 Social.propTypes = {
-  color: PropTypes.string,
-}
+  color: PropTypes.string
+};
 
-export default Social
+export default Social;
