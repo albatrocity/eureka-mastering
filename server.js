f3ea2e76d9e23a08225effb82902dbc2d40aa81d
Ross Brown <ross@cremalab.com>
2019-02-07 13:53:23 -0600

    Dep updates

--- server.js
+++ server.js
@@ -1,79 +1,91 @@
-require('dotenv').load()
-const express = require('express')
-const keystone = require('keystone')
-const next = require('next')
-const cookieParser = require('cookie-parser')
-const multer = require('multer')
-const body = require('body-parser')
-const serve = require('serve-static')
-const session = require('express-session')
-const api = require('./api')
-const cors = require('cors')
-const compression = require('compression')
+require("dotenv").load();
+const express = require("express");
+const keystone = require("keystone");
+const next = require("next");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const body = require("body-parser");
+const serve = require("serve-static");
+const session = require("express-session");
+const api = require("./api");
+const cors = require("cors");
+const compression = require("compression");
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const cookieSecret = process.env.COOKIE_SECRET || '123456789LETSEATPIEANDHAVEAGOODtime'
+const cookieSecret =
+  process.env.COOKIE_SECRET || "123456789LETSEATPIEANDHAVEAGOODtime";
 
-
-app.prepare()
+app
+  .prepare()
   .then(() => {
-    const server = express()
-    server.use(cookieParser(cookieSecret))
-    server.use(compression())
-    server.use(cors())
-    server.use(body.urlencoded({ extended: true }))
-    server.use(body.json())
-    server.use(multer())
-    server.use(session({
-      secret: cookieSecret
-    }))
+    const server = express();
+    server.use(cookieParser(cookieSecret));
+    server.use(compression());
+    server.use(cors());
+    server.use(body.urlencoded({ extended: true }));
+    server.use(body.json());
+    server.use(multer());
+    server.use(
+      session({
+        secret: cookieSecret
+      })
+    );
 
     keystone.init({
-      'name': 'Eureka Mastering',
-      'brand': 'Eureka Mastering',
-      'session store': 'connect-mongo',
-      'updates': 'updates',
-      'auth': true,
-      'user model': 'User',
-      'auto update': true,
-      'cookie secret': cookieSecret,
-      'admin path': 'admin',
-    })
+      name: "Eureka Mastering",
+      brand: "Eureka Mastering",
+      "session store": "connect-mongo",
+      updates: "updates",
+      auth: true,
+      "user model": "User",
+      "auto update": true,
+      "cookie secret": cookieSecret,
+      "admin path": "admin"
+    });
 
-    keystone.set('cloudinary config', {
+    keystone.set("cloudinary config", {
       cloud_name: process.env.CLOUDINARY_NAME,
       api_key: process.env.CLOUDINARY_KEY,
       api_secret: process.env.CLOUDINARY_SECRET
-    })
+    });
 
-    keystone.import('models')
-    server.use(serve('./public'))
+    keystone.import("models");
+    server.use(serve("./public"));
 
-    keystone.set('app', server)
-    server.use('/admin', require('keystone/admin/server/app/createStaticRouter.js')(keystone))
-    server.use('/admin', require('keystone/admin/server/app/createDynamicRouter.js')(keystone))
-    api(server, keystone)
+    keystone.set("app", server);
+    server.use(
+      "/admin",
+      require("keystone/admin/server/app/createStaticRouter.js")(keystone)
+    );
+    server.use(
+      "/admin",
+      require("keystone/admin/server/app/createDynamicRouter.js")(keystone)
+    );
+    api(server, keystone);
 
-    server.get('*', (req, res) => {
-      return handle(req, res)
-    })
-    server.get('/discography', (req, res) => {
-      return handle(req, res)
-    })
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+    server.get("/discography", (req, res) => {
+      return handle(req, res);
+    });
 
-    keystone.list('Configuration').model.update({active: true}, {
-      $set: { cache_key: Date.now()}
-    })
+    keystone.list("Configuration").model.update(
+      { active: true },
+      {
+        $set: { cache_key: Date.now() }
+      }
+    );
     // server.listen(3000, (err) => {
     //   if (err) throw err
     //   console.log('> Ready on http://localhost:3000')
     // })
-    keystone.start()
+    keystone.start();
   })
-  .catch((ex) => {
-    console.error(ex.stack)
-    process.exit(1)
-  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
--- store.js
+++ store.js
@@ -1,20 +1,20 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import {reducer as burgerMenu} from 'redux-burger-menu'
-import thunkMiddleware from 'redux-thunk'
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { reducer as burgerMenu } from "redux-burger-menu";
+import thunkMiddleware from "redux-thunk";
 
 const initialState = {
   audio_playing: false,
   audio_url: null,
   audio_current_position: 0,
-  audio_loading: false,
-}
+  audio_loading: false
+};
 
 export const actionTypes = {
-  AUDIO_PLAY: 'AUDIO_PLAY',
-  AUDIO_PAUSE: 'AUDIO_PAUSE',
-  AUDIO_SET_TIME: 'AUDIO_SET_TIME',
-  AUDIO_LOADING_SET: 'AUDIO_LOADING_SET',
-}
+  AUDIO_PLAY: "AUDIO_PLAY",
+  AUDIO_PAUSE: "AUDIO_PAUSE",
+  AUDIO_SET_TIME: "AUDIO_SET_TIME",
+  AUDIO_LOADING_SET: "AUDIO_LOADING_SET"
+};
 
 // REDUCERS
 export const appReducer = (state = initialState, action) => {
@@ -23,42 +23,44 @@ export const appReducer = (state = initialState, action) => {
       return Object.assign({}, state, {
         audio_playing: true,
         audio_loading: true,
-        audio_url: action.payload,
-      })
+        audio_url: action.payload
+      });
     case actionTypes.AUDIO_PAUSE:
       return Object.assign({}, state, {
-        audio_playing: false,
-      })
+        audio_playing: false
+      });
     case actionTypes.AUDIO_SET_TIME:
       return Object.assign({}, state, {
-        audio_current_position: action.payload.played,
-      })
+        audio_current_position: action.payload.played
+      });
     case actionTypes.AUDIO_LOADING_SET:
       return Object.assign({}, state, {
         audio_loading: action.payload
-      })
-    default: return state
+      });
+    default:
+      return state;
   }
-}
+};
 
 // ACTIONS
-export const playAudio = (url) => dispatch => {
-  return dispatch({ type: actionTypes.AUDIO_PLAY, payload: url })
-}
+export const playAudio = url => dispatch => {
+  return dispatch({ type: actionTypes.AUDIO_PLAY, payload: url });
+};
 export const pauseAudio = () => dispatch => {
-  return dispatch({ type: actionTypes.AUDIO_PAUSE })
-}
+  return dispatch({ type: actionTypes.AUDIO_PAUSE });
+};
 export const advanceAudio = () => dispatch => {
-  return dispatch({ type: actionTypes.AUDIO_SET_TIME })
-}
+  return dispatch({ type: actionTypes.AUDIO_SET_TIME });
+};
 export const setAudioLoading = () => dispatch => {
-  return dispatch({ type: actionTypes.AUDIO_LOADING_SET })
-}
+  return dispatch({ type: actionTypes.AUDIO_LOADING_SET });
+};
 
 const reducer = combineReducers({
-  app: appReducer, burgerMenu
-})
+  app: appReducer,
+  burgerMenu
+});
 
 export const initStore = (initialState = initialState) => {
-  return createStore(reducer, initialState, applyMiddleware(thunkMiddleware))
-}
+  return createStore(reducer, initialState, applyMiddleware(thunkMiddleware));
+};
